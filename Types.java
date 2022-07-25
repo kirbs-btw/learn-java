2a027036fba35b7fec75216f8eae2553b8aebab2
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-07-25 11:22:20 +0200

    Add files via upload

--- Types.java
+++ Types.java
@@ -1,8 +1,10 @@
 public class Types {
     public static void main(String[] args) {
         String text = "Hello World!";
-        
-
+        int num = 5;
+        float floNum = 5.2f;
+        char letter = 'F';
+        boolean bool = false;
 
         System.out.println(text);
     }
