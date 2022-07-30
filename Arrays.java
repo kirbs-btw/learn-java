9aee4d3152d5dcab1c6359f375de93efa57b9bd2
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-07-30 08:49:40 +0200

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,1 +1,15 @@
-// placeholder commits via phone
+public class Arrays {
+    public static void main(String[] args) {
+        String[] persons = {"Jim", "Bob", "Norm", "Gordon"};
+        int[] nums = {3, 0, 1, 2};
+
+        // System.out.println(persons[0]);
+        // System.out.println(nums[2]);
+        // System.out.println(persons.length);
+
+        for (int i = 0; i < persons.length; i++) {
+            System.out.println(nums[i]);
+            System.out.println(persons[nums[i]]);
+        }
+    }
+}
