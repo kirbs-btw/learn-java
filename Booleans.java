f276ca89c7ea08a77392b252e1ce0e2e9d9bd2fd
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-07-31 09:31:34 +0200

    Update Booleans.java

--- Booleans.java
+++ Booleans.java
@@ -1,1 +1,14 @@
-// placeholder commits via phone
+public class Booleans {
+    public static void main(String[] args) {
+        boolean isOn = true;
+        boolean isWorking = false;
+
+        int a = 10;
+        int b = 7;
+
+        System.out.println(a < b);
+        System.out.println(a != b);
+        System.out.println(a == b);
+
+    }
+}
