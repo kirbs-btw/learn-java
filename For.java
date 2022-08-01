d37c42cb16771bd22f565e8b78afc786ed6d05f0
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-01 09:45:39 +0200

    Update For.java

--- For.java
+++ For.java
@@ -1,1 +1,13 @@
-// placeholder commits via phone
+public class For {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+
+        for (int i = 0; i <= 10; i += 2){
+            System.out.println(i);
+        }
+
+    }
+}
