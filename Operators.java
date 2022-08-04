dc9783a3d6c0d0dd3d0b888c787d2247de4dd8a5
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-04 16:48:00 +0200

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -1,1 +1,27 @@
-// placeholder commits via phone
+public class Operators {
+    public static void main(String[] args) {
+        /*
+            +
+            -
+            *
+            /
+            %
+            ++ inc by one
+            -- dec by one
+        */
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(++a);
+
+        a += 5;
+
+        System.out.println(a);
+    }
+}
