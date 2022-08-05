d1deb91ea40475995f551f6f80c362ec798b45e2
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-05 08:37:02 +0200

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -1,1 +1,23 @@
-// placeholder commits via phone
+public class Strings {
+    public static void main(String[] args) {
+
+        String text = "hello sir THIS is cool";
+        System.out.println(text.length());
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        System.out.println(text.indexOf("THIS"));
+
+        String nameOne = "Smith";
+        String nameTwo = "Jeff";
+
+        System.out.println(nameOne + " " + nameTwo);
+        System.out.println(nameOne.concat(nameTwo));
+
+        String num = "50";
+        int numTwo = 10;
+        String a = num + numTwo;
+        System.out.println(a);
+
+
+    }
+}
