a81ca3fe82596800ba61afdd72541a2e4a8f9faf
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-06 05:32:48 +0200

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -1,1 +1,26 @@
-// placeholder commits via phone
+public class Switch {
+    public static void main(String[] args) {
+        // it's just fun i know there is a faster way ;)
+
+
+        int a = 3;
+
+        switch (a){
+            case 1:
+                System.out.println("Odd");
+                break;
+            case 2:
+                System.out.println("Even");
+                break;
+            case 3:
+                System.out.println("Odd");
+                break;
+            case 4:
+                System.out.println("Even");
+                break;
+            default:
+                System.out.println("That's not a number");
+        }
+
+    }
+}
