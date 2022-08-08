d8d34cbe7f32bc26dc9c1f0091ec8b6021ecbba9
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-08 14:27:39 +0200

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,28 @@
+
+
+public class Break {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            if (i == 4) {
+                break;
+            }
+            // System.out.println(i);
+        }
+
+        for (int i=0; i<10; i++){
+            if (i == 4) {
+                continue;
+            }
+            // System.out.println(i);
+        }
+
+        int i = 0;
+        while (i < 10){
+            System.out.println(i);
+            i++;
+            if (i == 4){
+                break;
+            }
+        }
+    }
+}
