7f2967c59a5e8a1bf280744dd143eaa0610855ce
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-10 17:31:42 +0200

    Add files via upload

---
+++ classes/Data.java
@@ -0,0 +1,14 @@
+package classes;
+
+/*
+ *
+ *  Defining some data in this class to use later in another file
+ *
+ */
+
+public class Data{
+    int[] numbers = {
+      1, 2, 3, 4, 5
+    };
+
+}
