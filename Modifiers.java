1265e6001ea879723cd734eee5a1e6986de13f06
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-12 17:32:23 +0200

    Add files via upload

---
+++ Modifiers.java
@@ -0,0 +1,29 @@
+public class Modifiers{
+
+    public static void main(String[] args) {
+        System.out.println("ello");
+
+        /*
+        * Access Modifiers:
+        *   - public        - code is accessible for all classes
+        *   - private       - code is only accessible for within the declared class
+        *   - protected     - code is accessible in the same package and subclasses
+        *
+        * Non-Access Modifiers:
+        *   - final         - class can not be inherited by other classes
+        *   - abstract      - class not for objects, an abstract calls must be inherited
+        *
+        *
+        * General Modifiers:
+        *   - final
+        *   - static
+        *   - abstract
+        *   - transient     - attributes/methods are skipped when serializing the object containing them
+        *   - synchronized  - methods only usable by one thread at a time
+        *   - volatile      - the value is not cached thread-locally, it's always main memory
+        */
+
+
+    }
+
+}
