c8d64dfad37dcac20b197a0c50196bbf2ee9b654
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-13 13:14:36 +0200

    Add files via upload

---
+++ encapsulation/Swap.java
@@ -0,0 +1,10 @@
+package encapsulation;
+
+public class Swap{
+    public static void main(String[] args) {
+        Ram dataObj = new Ram();
+        dataObj.setData("Jeff");
+        System.out.println(dataObj.getData());
+
+    }
+}
