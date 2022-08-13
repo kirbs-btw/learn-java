be5dc34d3dbe5b23dc84185479b271bc34025341
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-13 13:14:27 +0200

    Add files via upload

---
+++ encapsulation/Ram.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class Ram{
+    private String data;
+
+    // get method
+    public String getData(){
+        return this.data;
+    }
+
+    // set method
+    public void setData(String newData){
+        this.data = newData;
+    }
+
+}
