bd9dde827dcb0aea9048fd3048f3407c7e4c625a
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-14 13:50:12 +0200

    Add files via upload

---
+++ Api.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Api{
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username: ");
+
+        String userName = myObj.nextLine();
+        System.out.println(userName);
+    }
+
+}
