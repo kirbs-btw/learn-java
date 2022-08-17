6b180e3031e24d65ee8055af8aa4887b7b9c660e
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-17 16:52:10 +0200

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -2,17 +2,30 @@ class Animal {
     public void makeSound(){
         System.out.println("wuf");
     }
+
+    public int getAge() {
+        return 5;
+    }
 }
 
 class Bird extends Animal {
     public void makeSound(){
         System.out.println("weee");
     }
+
+    public int getAge() {
+        return 7;
+    }
+
 }
 
 class Cow extends Animal {
     public void makeSound(){
         System.out.println("reee");
+    }
+
+    public int getAge() {
+        return 35;
     }
 }
 
@@ -24,6 +37,8 @@ class Polymorphism {
         myAnimal.makeSound();
         myBird.makeSound();
         myCow.makeSound();
+
+        myCow.getAge();
         
     }
 }
