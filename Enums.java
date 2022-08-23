d4291fac08640d2ede816b9774bb003294e0673d
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-23 14:11:30 +0200

    Update Enums.java

---
+++ Enums.java
@@ -0,0 +1,14 @@
+// name to be changed 
+
+public class EnumsClass {
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
