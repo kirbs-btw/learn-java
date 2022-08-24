b025a41958d27900cfb1deaec30a72b974c8b913
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-24 10:17:58 +0200

    Update HashMap.java

---
+++ HashMap.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class HashMapObj{
+    public static void main(String[] args) {
+        HashMap<String, String> cities = new HashMap<String, String>();
+
+        cities.put("england", "london");
+        cities.put("germany", "berlin");
+        cities.put("luxemburg", "luxemburg");
+
+        System.out.println(cities.get("england"));
+
+        /*
+        * remove
+        * clear
+        * size
+        * get
+        * */
+
+
+    }
+
+
+}
