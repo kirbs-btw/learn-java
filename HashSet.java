a3fd87d08cd37da878ef71568713a1cc1583e98f
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-25 15:51:11 +0200

    Update HashSet.java

---
+++ HashSet.java
@@ -0,0 +1,13 @@
+// file name to be changed to HashSetObj
+
+import java.util.HashSet;
+
+public class HashSetObj{
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+
+        System.out.println(cars);
+    }
+}
