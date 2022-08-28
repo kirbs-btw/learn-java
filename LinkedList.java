4551d6f68aa7166f84ad6866ac898512ea0d72ae
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-08-28 08:11:57 +0200

    Update LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+
+public class LinkedListObj{
+    public static void main(String[] args) {
+
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Benz");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+    }
+
+
+}
