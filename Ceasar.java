6443c0b2b611b070e1e74281c4a5d20945a1e430
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-12-22 15:59:14 +0100

    Add files via upload

---
+++ Ceasar.java
@@ -0,0 +1,27 @@
+public class Ceasar{
+    String dic = "abcdefghijklmnopqrstuvwxyz ";
+
+    static String ceasar(String txt, int shift){
+        String newTxt = "";
+
+        for (int i = 0; i < txt.length(); i++){
+            int charASCII = (int) txt.charAt(i);
+            int new_ascii_num = (charASCII + shift) % 256;
+
+            char charLetter = (char) (new_ascii_num);
+            newTxt += charLetter;
+        }
+
+
+        return newTxt;
+    }
+
+
+    public static void main(String[] args){
+        String txt = "hello sir this is very cool to see that you are here!";
+        String a = ceasar(txt, 1);
+        System.out.println(a);
+        System.out.println(txt);
+
+    }
+}
