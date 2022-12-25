838efed0d272e06de9c09ecc34b5e5a4abaac7bc
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-12-25 08:17:52 +0100

    Add files via upload

---
+++ ui/MainFrame.java
@@ -0,0 +1,17 @@
+package ui;
+
+public class MainFrame extends javax.swing.JFrame{
+    
+    public static void main(String[] args) {
+        MainFrame frame=new MainFrame();
+        frame.setTitle("Hello World");
+        frame.setSize(1000, 620);
+        frame.setResizable(false);
+        frame.setLocation(50, 50);
+        frame.setVisible(true);
+
+
+    }
+}
+
+
