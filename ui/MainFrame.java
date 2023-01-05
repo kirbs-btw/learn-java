d2e492a3c9c8598b7064ecaea44570410ba91a11
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-01-05 17:41:40 +0100

    Add files via upload

--- ui/MainFrame.java
+++ ui/MainFrame.java
@@ -1,7 +1,9 @@
 package ui;
+import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends javax.swing.JFrame{
-    
+
     public static void main(String[] args) {
         MainFrame frame=new MainFrame();
         frame.setTitle("Hello World");
@@ -11,7 +13,14 @@ public class MainFrame extends javax.swing.JFrame{
         frame.setVisible(true);
 
 
+        JPanel panel = new JPanel();
+        JButton button = new JButton("click");
+        panel.add(button);
+        frame.add(panel);
+
+        frame.pack();
     }
+
 }
 
 
