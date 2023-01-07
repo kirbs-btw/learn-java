7381334740e3fea3c7ed2f511403812d25700238
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-01-07 13:59:11 +0100

    Add files via upload

---
+++ ui/Window.java
@@ -0,0 +1,35 @@
+package ui;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+class Window {
+
+    static JFrame frame;
+
+
+    static int count = 0;
+
+    public static void displaySome(){
+        System.out.println(count);
+        count++;
+    }
+
+    public static void main(String[] args) {
+        frame = new JFrame("Hello World");
+
+        JButton button = new JButton("click");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                displaySome();
+            }
+        });
+
+        frame.add(button);
+        frame.setSize(400, 400);
+        frame.show();
+    }
+
+}
